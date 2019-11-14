4673617ef9a9e8fafbd0f098ce6317029a5d7825
Brian Alden <brian.alden@colorado.edu>
2019-11-14 14:50:35 -0700

    Updated some files

--- circle/circle.py
+++ circle/circle.py
@@ -5,8 +5,10 @@ A demo project for APS Hack Day 2019
 Handles the primary functions
 """
 
+import numpy as np
+from numpy import pi as π
 
-def canvas(with_attribution=True):
+class Circle:
     """
     Placeholder function to show example docstring (NumPy format)
 
@@ -22,13 +24,22 @@ def canvas(with_attribution=True):
     quote : str
         Compiled string including quote and optional attribution
     """
+    def __init__(self, radius=1):
+        self.radius = radius
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+
+    def __str__(self):
+        return f"Circle(radius={self.radius})"
+    
+    @property
+    def area(self):
+        return π * (self.radius ** 2)
+
+    # @property
+    # def circumference(self):
+    #     return 2* π * self.radius
 
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
-    print(canvas())
+    print(Circle(5).area)
--- circle/tests/test_circle.py
+++ circle/tests/test_circle.py
@@ -6,7 +6,20 @@ Unit and regression test for the circle package.
 import circle
 import pytest
 import sys
+import numpy as np
+from numpy import pi as π
+
+test_radius = 5
 
 def test_circle_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "circle" in sys.modules
+
+def test_circle_radius():
+    test_radius = 5
+    test_area = π * (test_radius**2)
+    assert  test_area == circle.Circle(test_radius).area
+
+# def test_circle_circumference(test_circle):
+#     test_circumference = 2 * π * test_radius
+#     assert test_circumference == test_circle.circumference
